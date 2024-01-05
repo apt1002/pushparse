ef94c4590cdbf3f6a99c673a329f3601a2940132
Alistair Turnbull <apt1002@frog>
2024-01-05 22:23:58 +0000

    Move common functionality into trait `Wrapper`.

--- src/lib.rs
+++ src/lib.rs
@@ -3,12 +3,19 @@ use std::fmt::{Debug};
 mod utf8;
 pub use utf8::{Decoder};
 
+/// The type of parse errors.
 type E = &'static str;
 
+/// The push parser protocol.
+///
+/// To be useful, a type that implements `Parser` should also implement
+/// [`Push`] for at least one type of input token (other than [`E`]).
+///
+/// A `Parser` for which the concept of "end of file" makes sense should also
+/// implement `Flush`.
 pub trait Parser {
-    /// Insert an error token into the output, and reset this `Parser` to its
-    /// initial state.
-    fn push_error(&mut self, error: E);
+    /// Process `error`, possibly discarding some of the buffered input.
+    fn error(&mut self, error: E);
 }
 
 // ----------------------------------------------------------------------------
@@ -24,11 +31,15 @@ pub trait Push<T>: Parser {
 // ----------------------------------------------------------------------------
 
 /// [`Parser`]s that produce an output at the end of the input.
-pub trait Finish {
+pub trait Flush: Parser {
+    /// A completely parsed file (which may contain errors).
     type Output;
 
-    /// Feed "end of file" to this `Parser`, and retrieve the output.
-    fn finish(self) -> Self::Output;
+    /// Feed "end of file" to this `Parser`, forcing it to process any buffered
+    /// input, and retrieve the [`Output`].
+    ///
+    /// The `Parser` is reset to its initial state, and can be used again.
+    fn flush(&mut self) -> Self::Output;
 }
 
 // ----------------------------------------------------------------------------
@@ -40,27 +51,95 @@ pub struct Buffer<T> {
 }
 
 impl<T> Parser for Buffer<T> {
-    fn push_error(&mut self, error: E) { self.tokens.push(Err(error)); }
+    fn error(&mut self, error: E) { self.tokens.push(Err(error)); }
 }
 
 impl<T> Push<T> for Buffer<T> {
     fn push(&mut self, token: T) { self.tokens.push(Ok(token)); }
 }
 
-impl<T> Finish for Buffer<T> {
+impl<T> Flush for Buffer<T> {
     type Output = Box<[Result<T, E>]>;
 
-    fn finish(self) -> Self::Output { self.tokens.into() }
+    fn flush(&mut self) -> Self::Output { self.tokens.drain(..).collect() }
 }
 
 // ----------------------------------------------------------------------------
 
 /// [`Parser`]s that feed their output to another `Parser`.
-pub trait Wrap {
+///
+/// `Wrapper`s get a blanket implementations of several traits:
+/// - [`Parser`] itself.
+/// - [`Flush`], if [`Self::Inner`] implements `Flush`.
+/// - [`Push`] if `Self` implements [`MaybePush`].
+pub trait Wrapper: Parser {
+    /// The `Parser` that received the output of this `Wrapper`.
     type Inner: Parser;
 
     /// Returns the wrapped `Parser`.
     fn inner(&mut self) -> &mut Self::Inner;
+
+    /// Process any input buffered by this `Wrapper` and return it to its
+    /// initial state, but do not flush `self.inner()`.
+    fn partial_flush(&mut self);
+
+    /// Discard any input buffered by this `Wrapper`, and return it to its
+    /// initial state. `self.inner()` is unaffected.
+    fn partial_reset(&mut self);
+
+    /// An error message to use when this `Parser` is in its initial state and
+    /// receives an input token that it cannot accept.
+    ///
+    /// Typically the message will be "Syntax error: missing <expected input>".
+    const MISSING: E;
+}
+
+impl<P: Wrapper> Parser for P {
+    fn error(&mut self, error: E) {
+        self.partial_reset();
+        self.inner().error(error);
+    }
+}
+
+impl<P: Wrapper> Flush for P where P::Inner: Flush {
+    type Output = <P::Inner as Flush>::Output;
+
+    fn flush(&mut self) -> Self::Output {
+        self.partial_flush();
+        self.inner().flush()
+    }
+}
+
+// ----------------------------------------------------------------------------
+
+/// A simpler way to implement [`Push<T>`] for a [`Wrapper`].
+///
+/// The implementation of `push(token)` is equivalent to the following:
+/// ```text
+/// if let Some(token) = self.maybe_push(token) {
+///     self.partial_flush();
+///     if let Some(token) = self.maybe_push(token) {
+///         self.error(Self::MISSING);
+///     }
+/// }
+/// ```
+pub trait MaybePush<T>: Wrapper {
+    /// Try to feed `token` to this `Parser`.
+    ///
+    /// If `token` is accepted in the current state, perform any resulting
+    /// actions and return `None`, otherwise return `Some(token)`.
+    fn maybe_push(&mut self, token: T) -> Option<T>;
+}
+
+impl<T, P: Wrapper + MaybePush<T>> Push<T> for P {
+    fn push(&mut self, token: T) {
+        if let Some(token) = self.maybe_push(token) {
+            self.partial_flush();
+            if let Some(_) = self.maybe_push(token) {
+                self.error(Self::MISSING);
+            }
+        }
+    }
 }
 
 // ----------------------------------------------------------------------------
--- src/utf8.rs
+++ src/utf8.rs
@@ -1,4 +1,4 @@
-use super::{Parser, E, Push, Wrap, Finish};
+use super::{Parser, E, Push, MaybePush, Wrapper};
 
 /// The meaning of a UTF-8 byte.
 #[derive(Debug, Copy, Clone)]
@@ -16,15 +16,15 @@ const NEED1: Meaning = Meaning {mask: 0x1f, count: 1};
 const NEED2: Meaning = Meaning {mask: 0x0f, count: 2};
 const NEED3: Meaning = Meaning {mask: 0x07, count: 3};
 
-const MISSING: E = "Invalid UTF-8: missing continuation byte";
-const SPURIOUS: E = "Invalid UTF-8: spurious continuation byte";
-const INVALID: E = "Invalid unicode scalar value";
-
 /// The [`Meaning`]s of all non-continuation bytes, indexed by the top 4 bits.
 const MEANINGS: [Meaning; 0x10] = [
     ASCII, ASCII, ASCII, ASCII, ASCII, ASCII, ASCII, ASCII,
     DUMMY, DUMMY, DUMMY, DUMMY, NEED1, NEED1, NEED2, NEED3,
 ];
+
+const MISSING: E = "Invalid UTF-8: missing continuation byte";
+const SPURIOUS: E = "Invalid UTF-8: spurious continuation byte";
+const INVALID: E = "Invalid unicode scalar value";
 
 /// A [`Parser`] that accepts `u8`s and generates `char`s.
 #[derive(Default, Debug, Clone)]
@@ -45,29 +45,38 @@ impl<I: Push<char>> Decoder<I> {
     pub fn new(inner: I) -> Self { Decoder {inner, bits: 0, count: 0} }
 }
 
-impl<I: Push<char>> Wrap for Decoder<I> {
+impl<I: Push<char>> Wrapper for Decoder<I> {
     type Inner = I;
+
     fn inner(&mut self) -> &mut Self::Inner { &mut self.inner }
-}
 
-impl<I: Push<char>> Parser for Decoder<I> {
-    fn push_error(&mut self, error: E) {
-        self.count = 0;
-        self.inner().push_error(error);
+    fn partial_flush(&mut self) {
+        if self.count > 0 { self.error(MISSING); }
     }
+
+    fn partial_reset(&mut self) { self.count = 0; }
+
+    /// Any byte string is valid input.
+    const MISSING: E = "Should not happen";
 }
 
-impl<I: Push<char>> Push<u8> for Decoder<I> {
-    fn push(&mut self, token: u8) {
+impl<I: Push<char>> MaybePush<u8> for Decoder<I> {
+    fn maybe_push(&mut self, token: u8) -> Option<u8> {
         if token & 0xc0 == 0x80 {
             // It's a continuation byte.
-            if self.count == 0 { return self.push_error(SPURIOUS); }
+            if self.count == 0 {
+                self.error(SPURIOUS);
+                return None;
+            }
             let low_bits = token & 0x3f;
             self.bits = (self.bits << 6) | (low_bits as u32);
             self.count -= 1;
         } else {
             // It's not a continuation byte.
-            if self.count > 0 { self.push_error(MISSING); }
+            if self.count > 0 {
+                self.error(MISSING);
+                return Some(token); // Try again.
+            }
             let meaning = MEANINGS[token as usize >> 4];
             self.bits = (token & meaning.mask) as u32;
             self.count = meaning.count;
@@ -76,20 +85,10 @@ impl<I: Push<char>> Push<u8> for Decoder<I> {
             if let Some(c) = char::from_u32(self.bits) {
                 self.inner().push(c);
             } else {
-                return self.push_error(INVALID);
+                self.error(INVALID);
             }
         }
-    }
-}
-
-impl<I: Push<char> + Finish> Finish for Decoder<I> {
-    type Output = I::Output;
-
-    fn finish(mut self) -> Self::Output {
-        if self.count > 0 {
-            self.push_error(MISSING);
-        }
-        self.inner.finish()
+        None
     }
 }
 
@@ -98,12 +97,12 @@ impl<I: Push<char> + Finish> Finish for Decoder<I> {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::{Buffer};
+    use crate::{Buffer, Flush};
 
     fn check(input: &[u8], expected: &[Result<char, E>]) {
         let mut parser = Decoder::new(Buffer::default());
         for &byte in input { parser.push(byte); println!("parser = {:x?}", parser); }
-        let observed = parser.finish();
+        let observed = parser.flush();
         assert_eq!(expected, &*observed);
     }
 
